b31e475b786fd76ce23e722037973cf7675a1025
janthony25 <junanthony.business@gmail.com>
2024-02-04 13:11:57 +1300

    Add project files.

---
+++ OOP-Interface2/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace MyFirstProgram
+{
+    class Program
+    {
+
+        // interface - Practice
+        static void Main(string[] args)
+        {
+            Lebron lebron = new Lebron();
+            Ronaldo ronaldo = new Ronaldo();
+            Jun jun = new Jun();
+
+            // lebron.Shoot();
+            // ronaldo.Goal();
+            jun.Shoot();
+            jun.Goal();
+
+            Console.ReadKey();
+        }
+    }
+
+    interface IBasketball
+    {
+        void Shoot();
+    }
+
+    interface IFootball
+    {
+        void Goal();
+    }
+    
+    class Lebron : IBasketball
+    {
+        string name = "Lebron";
+
+        public void Shoot()
+        {
+            Console.WriteLine($"{name} shoots the ball");
+        }
+    }
+
+    class Ronaldo
+    {
+        string name = "Ronaldo";
+        public void Goal()
+        {
+            Console.WriteLine($"{name} kicks the boal towards the goal");
+        }
+    }
+
+    class Jun : IBasketball, IFootball
+    {
+        string name = "Jun";
+        public void Shoot()
+        {
+            Console.WriteLine($"{name} shoots the ball");
+        }
+        
+        public void Goal()
+        {
+            Console.WriteLine($"{name} kicks the ball");
+        }
+    }
+}
